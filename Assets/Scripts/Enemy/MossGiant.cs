30cb2273d8e98936f885899a7a7c58da9363d166
unknown <mattsigfrid@gmail.com>
2020-09-29 10:39:35 -0400

    added skele damage

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MossGiant : Enemy
+public class MossGiant : Enemy, IDamageable
 {
-    public int height;
+    public int Health { get; set; }
 
     public override void Init()
     {
         base.Init();
+        Health = base.health;
+    }
+
+    public void Damage()
+    {
+
     }
 }
--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : Enemy
+public class Skeleton : Enemy, IDamageable
 {
+    public int Health { get; set; }
     public override void Init()
     {
         base.Init();
+
+        Health = base.health;
+    }
+
+    public void Damage()
+    {
+        Debug.Log("Damage");
+
+        Health--;
+        if (Health < 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
 }
