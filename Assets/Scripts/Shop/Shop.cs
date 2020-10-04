9094aad4e54a3a83d53b369479b678843145ccfd
unknown <mattsigfrid@gmail.com>
2020-10-04 13:44:14 -0400

    added shopkepper

---
+++ Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPanel;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            shopPanel.SetActive(true);
+        }
+    }
+}
